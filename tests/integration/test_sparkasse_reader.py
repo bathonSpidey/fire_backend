7422589c93d20a7127736104ec0d8ba8ce7a9646
Abir Bhattacharyya <46240907+bathonSpidey@users.noreply.github.com>
2026-06-14 09:58:54 +0200

    WIP N26

--- tests/integration/test_sparkasse_reader.py
+++ tests/integration/test_sparkasse_reader.py
@@ -1,22 +1,8 @@
 import pathlib
 
-from pydantic import BaseModel
-
 from Readers.SparkasseReader import SparkasseReader
 
 file_path = pathlib.Path(__file__).parent / "data" / "statement.pdf"
-
-
-class BankStatement(BaseModel):
-    date: str
-    bank: str
-    balance_last_month: float
-
-    amount: float
-    balance: float
-
-
-# --- Testing Setup (e.g., for pytest) ---
 
 
 class TestSparkasseReader:
